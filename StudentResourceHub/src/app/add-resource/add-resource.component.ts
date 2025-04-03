d8c806d849446cfca6ec8d933ff76286df7ef4f9
NelhomeSagoco <20161045@s.ubaguio.edu>
2025-04-03 14:54:20 +0800

    Adding Other Components

---
+++ StudentResourceHub/src/app/add-resource/add-resource.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-resource',
+  imports: [],
+  templateUrl: './add-resource.component.html',
+  styleUrl: './add-resource.component.css'
+})
+export class AddResourceComponent {
+
+}
